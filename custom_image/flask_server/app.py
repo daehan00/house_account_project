d6b8c364fdcec6a78148aecf577ed95adc52c433
dhkim <rlaeogks4682@gmail.com>
2024-12-04 23:45:27 +0900

    회의록 한글파일 다운로드

--- custom_image/flask_server/app.py
+++ custom_image/flask_server/app.py
@@ -363,10 +363,11 @@ def download_file():
         directory = os.getenv("UPLOAD_FOLDER_RA") + f"/{house_name}/etc"
     elif file_type == 'minute':
         directory = os.getenv("UPLOAD_FOLDER_RA") + f"/{house_name}/minutes"
-        if pdf_filename:
-            filename = filename + '.pdf'
-        else:
-            filename = filename + '.hwp'
+        # if pdf_filename:
+        #     filename = filename + '.pdf'
+        # else:
+        #     filename = filename + '.hwp'
+        filename = filename + '.hwp'
     elif file_type == 'setup':
         directory = os.getenv("UPLOAD_FOLDER_TMP")
     else:
