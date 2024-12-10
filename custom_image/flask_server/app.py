6287cfcb3816725301414d09cbcb78179769aaf7
dhkim <rlaeogks4682@gmail.com>
2024-12-10 16:38:45 +0900

    subprocess 수정

--- custom_image/flask_server/app.py
+++ custom_image/flask_server/app.py
@@ -720,9 +720,9 @@ def webhook():
     if request.method == 'POST':
         payload = request.json
         if payload and payload['ref'] == 'refs/heads/main':
-            subprocess.run(["sudo", "git", "pull", "origin", "main"])
-            subprocess.run(["sudo", "docker-compose", "pull", "flask-app"])
-            subprocess.run(["sudo", "docker-compose", "up", "-d", "flask-app"])
+            subprocess.run(["git", "pull", "origin", "main"])
+            subprocess.run(["docker-compose", "pull", "flask-app"])
+            subprocess.run(["docker-compose", "up", "-d", "flask-app"])
             return 'Webhook received and Docker updated', 200
         else:
             return 'Not the main branch', 200
