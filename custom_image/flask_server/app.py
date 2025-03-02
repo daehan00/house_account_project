b515848301a34e50b2f237235f3c2f6e0649ac46
dhkim <rlaeogks4682@gmail.com>
2025-03-03 01:17:25 +0900

    error fix

--- custom_image/flask_server/app.py
+++ custom_image/flask_server/app.py
@@ -749,15 +749,18 @@ def delete_minutes():
 
 @app.route("/ra/download/template", methods=['POST']) #양식파일 다운로드
 def download_template():
-    if session.get('ra') or session.get('manager'):
+    if not (session.get('ra') or session.get('manager')):
+        flash("권한이 없습니다.", "warning")
+        return redirect("/")
+    else:
         category = request.form.get('category')
         data_dir = os.getenv("DATA_PATH")
         if category == 'receipt':
-            file_name = "241001(영)_000RA_프로그램명_심야.xlsx"
+            file_name = "250000(영)_OOORA_AVISON 프로그램명_심야.xlsx"
         elif category == 'minutes':
-            file_name = "240000(회)_OOORA_AVISON 프로그램명.hwp"
+            file_name = "250000(회)_OOORA_AVISON 프로그램명.hwp"
         elif category == 'gift':
-            file_name = "240902(기)_000RA_프로그램명.xlsx"
+            file_name = "250000(기)_OOORA_AVISON 프로그램명.xlsx"
         else:
             flash("잘못된 요청입니다.", 'error')
             return redirect("/")
